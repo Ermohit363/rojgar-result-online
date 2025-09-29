8100e0dcfab8bd8b83fe7ebb94e29b220cafd3a0
Ermohit363 <mohit.cs2016@gmail.com>
2025-09-29 22:17:08 +0530

    content.nuxt file update

--- content.config.js
+++ content.config.js
@@ -1,14 +1,19 @@
 import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { asSeoCollection } from '@nuxtjs/seo/content'
 
 export default defineContentConfig({
   collections: {
-   posts: defineCollection({
-      type: 'page',
-      source: 'posts/**/*.md'
-    }),
-    currentAffairs: defineCollection({
-      type: 'page',
-      source: 'current-affairs/**/*.md'
-    })
+    posts: defineCollection(
+      asSeoCollection({
+        type: 'page',
+        source: 'posts/**/*.md'
+      })
+    ),
+    currentAffairs: defineCollection(
+      asSeoCollection({
+        type: 'page',
+        source: 'current-affairs/**/*.md'
+      })
+    )
   }
 })
