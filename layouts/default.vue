a21d93b6f4a9a394c01bc239c72e637fa6bd67d1
Ermohit363 <mohit.cs2016@gmail.com>
2025-10-09 22:51:23 +0530

    some seo added

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,6 +38,7 @@ useHead({
     //{ property: 'og:title', content: 'Rojgar Result Online - Sarkari Result Online & Govt Job Updates' },
    // { property: 'og:description', content: 'Get latest Rojgar Result Online updates including Rojgar Result, Sarkari Results, Govt Jobs, Admit Cards, Daily Current Affairs and Recruitment 2025 notifications.' },
     { property: 'og:site_name', content: `${useRuntimeConfig().public.baseUrl}` },
+    { hid: 'og:image', property: 'og:image', content: 'https://www.rojgarresultonline.com/rojgar-result-online-new.png' },
     { name: "google-site-verification", content: "nAu6Mw-j_7wsDOkPoBtw2GCnn7pl9JMxkLOH7jMBEKI" }
   ],
   link: [{ rel: 'canonical',  href: 'https://www.rojgarresultonline.com' + route.path }]
